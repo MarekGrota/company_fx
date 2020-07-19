6437d2c1040f07fdbbc9aa16e3289fc099c848db
Marek Grota <marekgro@gmail.com>
2020-07-19 15:00:27 +0200

    add color login and password frame in case of statement of login

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -8,6 +8,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import utility.UserData;
 
 public class LoginController {
 
@@ -19,7 +20,23 @@ public class LoginController {
 
     @FXML
     void loginAction(ActionEvent event) {
-
+        boolean isLogged = UserData.users.stream()
+                .anyMatch(user ->
+                        user.getEmail().equals(pfLogin.getText()) &&
+                        user.getPassword().equals(pfPassword.getText()));
+        if(isLogged){
+            System.out.println("Zalogowano");
+            pfLogin.setStyle(null);
+            pfPassword.setStyle(null);
+            pfLogin.setStyle("-fx-border-color: green; -fx-border-width: 3px");
+            pfPassword.setStyle("-fx-border-color: green; -fx-border-width: 3px");
+        } else {
+            System.out.println("niezalogowano");
+            pfLogin.clear();
+            pfPassword.clear();
+            pfLogin.setStyle("-fx-border-color: red; -fx-border-width: 3px");
+            pfPassword.setStyle("-fx-border-color: red; -fx-border-width: 3px");
+        }
     }
 
 }
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -5,7 +5,7 @@ import lombok.*;
 import java.time.LocalDateTime;
 
 @Data
-@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private String email;
     private String password;
