cf4df275bc2b98d582a05e184a1f708c1acdc40a
Marek Grota <marekgro@gmail.com>
2020-08-02 09:54:09 +0200

    add login action after pressing enter key and close window by pressing escape key

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -4,10 +4,13 @@
 
 package controller;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import utility.UserData;
 
 public class LoginController {
@@ -18,17 +21,16 @@ public class LoginController {
     @FXML // fx:id="pfPassword"
     private PasswordField pfPassword; // Value injected by FXMLLoader
 
-    @FXML
-    void loginAction(ActionEvent event) {
+    private void login () {
         boolean isLogged = UserData.users.stream()
                 .anyMatch(user ->
                         user.getEmail().equals(tfLogin.getText()) &&
-                        user.getPassword().equals(pfPassword.getText()));
-        if(isLogged){
+                                user.getPassword().equals(pfPassword.getText()));
+        if (isLogged) {
             System.out.println("Zalogowano");
             tfLogin.setStyle(null);
             pfPassword.setStyle(null);
-            tfLogin.clear();
+            //tfLogin.clear();
             pfPassword.clear();
             tfLogin.setStyle("-fx-border-color: green; -fx-border-width: 2px");
             pfPassword.setStyle("-fx-border-color: green; -fx-border-width: 2px");
@@ -41,4 +43,15 @@ public class LoginController {
         }
     }
 
+    @FXML // klikniecie na button (button)
+    void loginAction(ActionEvent event) {
+    }
+
+    @FXML //wciśnięcie Enter w dowolnym miejscu na oknie aplikacji (vbox)
+    void keyLoginAction(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER)
+            login();
+        if (event.getCode() == KeyCode.ESCAPE)
+            Platform.exit(); // zamknięcie okna aplikacji (wywołanie metody close())
+    }
 }
