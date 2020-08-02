07503da6a7e390433b50cf08ef980ae516a2739b
Marek Grota <marekgro@gmail.com>
2020-08-02 12:15:57 +0200

    add logout and close in wharehouse window

--- src/main/java/controller/CompanyController.java
+++ src/main/java/controller/CompanyController.java
@@ -1,4 +1,105 @@
+/**
+ * Sample Skeleton for 'companyView.fxml' Controller Class
+ */
+
 package controller;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class CompanyController {
+
+    @FXML // fx:id="tblProduct"
+    private TableView<?> tblProduct; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tcName"
+    private TableColumn<?, ?> tcName; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tcCategory"
+    private TableColumn<?, ?> tcCategory; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tcPrice"
+    private TableColumn<?, ?> tcPrice; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tcQuantity"
+    private TableColumn<?, ?> tcQuantity; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tfSearch"
+    private TextField tfSearch; // Value injected by FXMLLoader
+
+    @FXML // fx:id="cbLess5"
+    private CheckBox cbLess5; // Value injected by FXMLLoader
+
+    @FXML // fx:id="cbMedium"
+    private CheckBox cbMedium; // Value injected by FXMLLoader
+
+    @FXML // fx:id="cbMore10"
+    private CheckBox cbMore10; // Value injected by FXMLLoader
+
+    @FXML // fx:id="comboCategory"
+    private ComboBox<?> comboCategory; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnUpdate"
+    private Button btnUpdate; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnDelete"
+    private Button btnDelete; // Value injected by FXMLLoader
+
+    @FXML
+    void addAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void closeAction(ActionEvent event) {
+        Platform.exit();
+    }
+
+    @FXML
+    void deleteAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void filterAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logOutAction(ActionEvent event) throws IOException {
+        Stage primaryStage = new Stage();
+        Parent parent = FXMLLoader.load(getClass().getResource("/view/loginView.fxml"));
+        primaryStage.setTitle("Logowanie");             // tytuł okna
+        primaryStage.setResizable(false);               // brak skalowania
+        primaryStage.setScene(new Scene(parent));
+        primaryStage.show();
+        // zamknięcie okna logowania na obiekcie typu Stage
+        Stage companyStage = (Stage) btnDelete.getScene().getWindow();
+        companyStage.close();
+    }
+
+    @FXML
+    void selectAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void updateAction(ActionEvent event) {
+
+    }
+
 }
--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -7,11 +7,18 @@ package controller;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import utility.UserData;
+
+import java.io.IOException;
 
 public class LoginController {
 
@@ -21,7 +28,7 @@ public class LoginController {
     @FXML // fx:id="pfPassword"
     private PasswordField pfPassword; // Value injected by FXMLLoader
 
-    private void login () {
+    private void login() throws IOException {
         boolean isLogged = UserData.users.stream()
                 .anyMatch(user ->
                         user.getEmail().equals(tfLogin.getText()) &&
@@ -30,10 +37,20 @@ public class LoginController {
             System.out.println("Zalogowano");
             tfLogin.setStyle(null);
             pfPassword.setStyle(null);
-            //tfLogin.clear();
+
+            // wywołanie nowego okna aplikacji
+            Stage primaryStage = new Stage();
+            Parent parent = FXMLLoader.load(getClass().getResource("/view/companyView.fxml"));
+            primaryStage.setTitle("Aplikacja magazynowa");  // tytuł okna
+            primaryStage.setResizable(false);               // brak skalowania
+            primaryStage.initStyle(StageStyle.UNDECORATED); // brak przycisków w tytule okna
+            primaryStage.setScene(new Scene(parent));
+            primaryStage.show();
             pfPassword.clear();
-            tfLogin.setStyle("-fx-border-color: green; -fx-border-width: 2px");
-            pfPassword.setStyle("-fx-border-color: green; -fx-border-width: 2px");
+
+            // zamknięcie okna logowania na obiekcie typu String
+            Stage loginStage = (Stage) tfLogin.getScene().getWindow();
+            loginStage.close();
         } else {
             System.out.println("niezalogowano");
             tfLogin.clear();
@@ -43,12 +60,15 @@ public class LoginController {
         }
     }
 
-    @FXML // klikniecie na button (button)
-    void loginAction(ActionEvent event) {
+    @FXML
+        // klikniecie na button (button)
+    void loginAction(ActionEvent event) throws IOException {
+        login();
     }
 
-    @FXML //wciśnięcie Enter w dowolnym miejscu na oknie aplikacji (vbox)
-    void keyLoginAction(KeyEvent event) {
+    @FXML
+        //wciśnięcie Enter w dowolnym miejscu na oknie aplikacji (vbox)
+    void keyLoginAction(KeyEvent event) throws IOException {
         if (event.getCode() == KeyCode.ENTER)
             login();
         if (event.getCode() == KeyCode.ESCAPE)
